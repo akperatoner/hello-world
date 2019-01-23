d4b35c95b8809bcaafa339ff6be7fa9536245c33
akperatoner <44481869+akperatoner@users.noreply.github.com>
2019-01-22 21:06:33 -0800

    Update peratonerLab3.cpp

--- peratonerLab3.cpp
+++ peratonerLab3.cpp
@@ -1,7 +1,7 @@
 // Author Your Name
 // Description: Hello World in C++
 //============================================================
-main#include <iostream>
+#include <iostream>
   using namespace std;
   int hello1Function(string name1);
   int hello2Function(string name2);
